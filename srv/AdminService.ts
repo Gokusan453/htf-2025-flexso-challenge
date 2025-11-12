07278c1c206d1685521678c095fc0fe697fea4ba
Gokusan453 <gokhangunerr4@gmail.com>
2025-11-12 11:15:55 +0100

    admin-service

--- srv/AdminService.ts
+++ srv/AdminService.ts
@@ -1,4 +1,5 @@
 import * as cds from "@sap/cds";
+import type { Request } from "@sap/cds";
 import * as handlerSymbol from "./handlers/symbols";
 import * as handlerMaterial from "./handlers/materials";
 import * as handlerCamera from "./handlers/camera";
@@ -15,29 +16,29 @@ export = (srv: cds.Service) => {
     return data;
   });
 
-  srv.on("translateSymbolBound", "Symbols", (req: any) => {
+  srv.on("translateSymbolBound", "Symbols", (req: Request) => {
     return handlerSymbol.translate(req);
   });
 
-  srv.on("order", "Materials", (req: any) => {
+  srv.on("order", "Materials", (req: Request) => {
     return handlerMaterial.order(req);
   });
 
-  srv.on("produce", "ProductCamera", (req: any) => {
+  srv.on("produce", "ProductCamera", (req: Request) => {
     return handlerMaterial.produce(req);
   });
 
-  srv.on("replace", "Installation", (req: any) => {
+  srv.on("replace", "Installation", (req: Request) => {
     return handlerMaterial.replaceInstallation(req);
   });
 
-  srv.before("READ", "CameraImages", req => handlerCamera.checkCameraAvailability(req));
+  srv.before("READ", "CameraImages", (req: Request) => handlerCamera.checkCameraAvailability(req));
 
-  srv.on("checkCameraAvailability", async (req) => {
+  srv.on("checkCameraAvailability", async (req: Request) => {
     return handlerCamera.areAllCamerasAvailable(req);
   });
 
-  srv.on("getCamerarecordingIdForLocation", async (req) => {
+  srv.on("getCamerarecordingIdForLocation", async (req: Request) => {
     return handlerCamera.getCamerarecordingIdForLocation(req);
   });
 
